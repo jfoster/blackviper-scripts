5fdfd5d8b402d2be2055900a0b05adbca61e53b2
jfoster <jfoster@users.noreply.github.com>
2018-10-11 15:11:06 +0100

    Add winxp-sp3 service config scripts

---
+++ winxp-sp3 - DEFAULT Home.ps1
@@ -0,0 +1,82 @@
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "alg.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "clipsrv.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "dllhost.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "msdtc.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "services.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "inetinfo.exe" -StartupType Not Available 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Disabled 
+Set-Service "inetinfo.exe" -StartupType Not Available 
+Set-Service "imapi.exe" -StartupType Manual 
+Set-Service "cisvc.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "lsass.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "dmadmin.exe" -StartupType Manual 
+Set-Service "mqsvc.exe" -StartupType Not Available 
+Set-Service "mqtgsvc.exe" -StartupType Not Available 
+Set-Service "services.exe" -StartupType Automatic 
+Set-Service "dllhost.exe" -StartupType Manual 
+Set-Service "lsass.exe" -StartupType Manual 
+Set-Service "mnmsrvc.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "netdde.exe" -StartupType Manual 
+Set-Service "netdde.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "lsass.exe" -StartupType Manual 
+Set-Service "smlogsvc.exe" -StartupType Manual 
+Set-Service "services.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "spoolsv.exe" -StartupType Automatic 
+Set-Service "lsass.exe" -StartupType Automatic 
+Set-Service "rsvp.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "sessmgr.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "locator.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Not Available 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Disabled 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "lsass.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "inetinfo.exe" -StartupType Not Available 
+Set-Service "SCardSvr.exe" -StartupType Manual 
+Set-Service "SCardSvr.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "tlntsvr.exe" -StartupType Not Available 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "ups.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "vssvc.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Manual 
+Set-Service "msiexec.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Not Available 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "wmiapsrv.exe" -StartupType Manual 
+Set-Service "svchost.exe" -StartupType Automatic 
+Set-Service "inetinfo.exe" -StartupType Not Available 
